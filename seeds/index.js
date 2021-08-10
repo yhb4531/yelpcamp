7b49813a06a2a5109779d14de7286fdd8353a6bd
yhb4531 <55854538+yhb4531@users.noreply.github.com>
2021-08-10 00:12:22 -0600

    Styling added

--- seeds/index.js
+++ seeds/index.js
@@ -21,9 +21,14 @@ const seedDB =async () => {
     await Campground.deleteMany({});
     for(let i=0; i<50; i++){
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 30 + 10);
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque itaque consequuntur numquam voluptatibus cupiditate accusamus eum, sit temporibus dicta, aliquam architecto officia facere, cum mollitia nihil harum? Aperiam, quod eius.',
+            //Shorthand of price:price
+            price
         })
         await camp.save();
     }
