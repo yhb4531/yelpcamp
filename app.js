242eba42ae9d9e76a4b2a6d90d4b6d463e1f1124
yhb4531 <55854538+yhb4531@users.noreply.github.com>
2021-08-26 01:08:06 -0600

    deploying

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ const User = require('./models/user');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 
+const MongoDBStore = require('connect-mongo');
+
 // const dbUrl = process.env.DB_URL;
 
 //Separated out all the paths using routes
@@ -27,8 +29,8 @@ const reviewRoute = require('./routes/reviews');
 const { contentSecurityPolicy } = require('helmet');
 
 
-// 'mongodb://localhost:27017/yelp-camp'
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
@@ -54,9 +56,24 @@ app.use(mongoSanitize({
     replaceWith: '_'
 }));
 
+const secret = process.env.SECRET || 'thisshouldbeabettersecret';
+
+const store = MongoDBStore.create({
+    mongoUrl: dbUrl,
+    touchAfter: 24 * 60 * 60,
+    crypto: {
+        secret
+    }
+});
+
+store.on("error", function(e){
+    console.log("SESSION STORE ERROR", e);
+});
+
 const sessionConfig = {
+    store,
     name: 'session',
-    secret: 'thisshouldbeabettersecret',
+    secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
@@ -66,7 +83,7 @@ const sessionConfig = {
         maxAge: 1000 * 60 * 60 * 25 * 7
     }
 }
-app.use(session(sessionConfig));    
+app.use(session(sessionConfig));
 app.use(flash());
 app.use(helmet());
 
