6ab408f9c00d6f697a244d12df5a42ac8bd4d37f
yhb4531 <55854538+yhb4531@users.noreply.github.com>
2021-08-26 01:50:30 -0600

    dburl update

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ const reviewRoute = require('./routes/reviews');
 const { contentSecurityPolicy } = require('helmet');
 
 
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+const dbUrl = process.env.DB_URL;
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useCreateIndex: true,
