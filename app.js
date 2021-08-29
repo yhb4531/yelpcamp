c7744b2b21087b2b577630dd0e437d5309941392
yhb4531 <55854538+yhb4531@users.noreply.github.com>
2021-08-29 01:12:59 -0600

    typo

--- app.js
+++ app.js
@@ -165,7 +165,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err });
 })
 
-const port = process.nextTick.PORT || 3000
+const port = process.env.PORT || 3000
 
 app.listen(port, () => {
     console.log(`Serving on Port ${port}`);
